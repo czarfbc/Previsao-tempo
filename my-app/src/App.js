af9817b46fd3081e9106cd7f7ce51ac6e0529f4e
czarfbc <cezarfbc@gmail.com>
2022-12-16 16:42:28 -0300

    Previsão do tempo.
    Previsão do tempo usando API openweathermap, praticamente sem estilização

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <Search placeholder='Digite a cidade...'/>
+      <Search placeholder='Enter the city...'/>
     </div>
   );
 }
--- my-app/src/Search.js
+++ my-app/src/Search.js
@@ -1,8 +1,12 @@
+import {useState} from 'react';
 import axios from "axios";
 
 function Search(props){
+    const [city, setCity] = useState("");
+
     function searchInput(e){
         e.preventDefault();
+
         let currentValue = document.querySelector('input[name=searchInput]').value;
 
         const url = `https://api.openweathermap.org/data/2.5/weather?q=${currentValue}&appid=4d8fb5b93d4af21d66a2948710284366&units=metric`;
@@ -16,20 +20,52 @@ function Search(props){
                     res.data.sys.country,
                 description: 
                     res.data.weather[0].description,
-                
+                temp:
+                    res.data.main.temp,
+                sys: 
+                    res.data.sys,
+                weather:
+                    res.data.weather,   
             }   
-            console.log(`${info.name}-${info.country}`)
-            console.log(info.description)
+
+
+
+            if(info.sys !== undefined){
+                if(info.weather !== undefined){
+
+                    const icon = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${info.weather[0]["icon"]}.svg`;
+
+                    setCity(`
+                    <div>
+                        <p>Temperature: ${info.temp}°C</p>
+                        <p>Country: ${info.country}</p>
+                        <p>City: ${info.name}</p>
+                        <p>Description: ${info.weather[0]['description']}</p>
+                        <img src="${icon}" />
+    
+                    </div>
+    
+                    `);
+                }
+            }else{setCity("")}
         })
     }
 
     return(
-        <div className="search">
-            <h2>Digite a cidade que você quer saber a previsão</h2>
-            <form onSubmit={(e)=>searchInput(e)}>
-                <input placeholder={props.placeholder} onSubmit={searchInput} type="text" name="searchInput"/>
-                <input type="submit" value="Pesquisar por cidade!" />
-            </form>
+        <div className='searchWraper'>
+            <div className="search">
+                <h2>Enter the city you want to know the forecast.</h2>
+                <form onSubmit={(e)=>searchInput(e)}>
+                    <input placeholder={props.placeholder} onSubmit={searchInput} type="text" name="searchInput"/>
+                    <input type="submit" value="Search by city!" />
+                </form>
+            </div>
+
+            {
+                (city !== "")?
+                <div dangerouslySetInnerHTML={{__html: city}}/>:
+                <div><p>Search for something above...</p></div>
+            }
         </div>
     )
 }
